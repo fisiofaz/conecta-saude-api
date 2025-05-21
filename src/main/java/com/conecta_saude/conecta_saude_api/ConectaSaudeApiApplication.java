f590d113857d96fd4e8704ceb09ba915b85537bd
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-05-20 22:28:21 -0300

    feat: Initial project setup with Spring Boot and basic configurations

---
+++ src/main/java/com/conecta_saude/conecta_saude_api/ConectaSaudeApiApplication.java
@@ -0,0 +1,13 @@
+package com.conecta_saude.conecta_saude_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaSaudeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaSaudeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conecta_saude/conecta_saude_api/ConectaSaudeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conecta_saude.conecta_saude_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaSaudeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
