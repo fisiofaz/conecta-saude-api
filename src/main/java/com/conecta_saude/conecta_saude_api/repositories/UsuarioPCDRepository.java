b3ba5654cdd731d1d79d5df12d4b9708a3d5a6d7
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-05-21 23:52:06 -0300

    feat: implemetação do CRUD UsuarioPCD

--- src/main/java/com/conecta_saude/conecta_saude_api/repositories/UsuarioPCDRepository.java
+++ src/main/java/com/conecta_saude/conecta_saude_api/repositories/UsuarioPCDRepository.java
@@ -1,10 +1,14 @@
 package com.conecta_saude.conecta_saude_api.repositories;
 
 import com.conecta_saude.conecta_saude_api.models.UsuarioPCD;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository 
 public interface UsuarioPCDRepository extends JpaRepository<UsuarioPCD, Long> {
-    
+	List<UsuarioPCD> findByTipoDeficiencia(String tipoDeficiencia);
+	List<UsuarioPCD> findByCidade(String cidade);
 }
